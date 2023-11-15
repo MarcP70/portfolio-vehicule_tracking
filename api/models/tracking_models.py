26bc8af38d92a0ea1ce352a05143bcbda9caffee
MarcP70 <marcpourias@yahoo.fr>
2023-11-15 11:08:31 +0100

    fix file tracking_models.py to add docstrings

--- api/models/tracking_models.py
+++ api/models/tracking_models.py
@@ -1,15 +1,43 @@
 import uuid
 from api.app import db
 
+
 class TrackingCreationError(Exception):
+    """
+    Custom exception class that is raised when there is an error during the
+        creation of a tracking entry.
+    """
     pass
+
 
 class TrackingDeletionError(Exception):
+    """
+    Custom exception class that is raised when there is an error during the
+        deletion of a tracking entry.
+    """
     pass
 
+
 class Tracking:
+    """
+    Class for managing tracking information for vehicles.
+    """
 
     def create(self, vehicle_id, **kwargs):
+        """
+        Creates a new tracking entry for a vehicle.
+
+        Args:
+            vehicle_id (str): The ID of the vehicle.
+            **kwargs: Additional keyword arguments for the tracking entry.
+
+        Returns:
+            dict: The created tracking entry.
+
+        Raises:
+            TrackingCreationError: If there is an error during the creation of
+                the tracking entry.
+        """
         tracking_id = uuid.uuid4().hex
         tracking = {
             "_id": tracking_id,
@@ -22,22 +50,73 @@ class Tracking:
         return tracking
 
     def read_by_vehicle(self, vehicle_id):
+        """
+        Retrieves all tracking entries for a vehicle.
+
+        Args:
+            vehicle_id (str): The ID of the vehicle.
+
+        Returns:
+            list: A list of tracking entries.
+        """
         return list(db.trackings.find({"vehicle_id": vehicle_id}))
 
     def read(self, tracking_id):
+        """
+        Retrieves a specific tracking entry.
+
+        Args:
+            tracking_id (str): The ID of the tracking entry.
+
+        Returns:
+            dict: The tracking entry.
+        """
         return db.trackings.find_one({"_id": tracking_id})
 
     def delete_by_vehicle(self, vehicle_id):
+        """
+        Deletes all tracking entries for a vehicle.
+
+        Args:
+            vehicle_id (str): The ID of the vehicle.
+
+        Raises:
+            TrackingDeletionError: If no tracking entries are found for the
+                vehicle.
+        """
         result = db.trackings.delete_many({"vehicle_id": vehicle_id})
         if result.deleted_count == 0:
             raise TrackingDeletionError("Aucun suivi trouvé pour ce véhicule")
 
     def delete(self, tracking_id):
+        """
+        Deletes a specific tracking entry.
+
+        Args:
+            tracking_id (str): The ID of the tracking entry.
+
+        Raises:
+            TrackingDeletionError: If the tracking entry is not found or
+                already deleted.
+        """
         result = db.trackings.delete_one({"_id": tracking_id})
         if result.deleted_count == 0:
             raise TrackingDeletionError("Suivi introuvable ou déjà supprimé")
 
     def update(self, tracking_id, **kwargs):
+        """
+        Updates a specific tracking entry.
+
+        Args:
+            tracking_id (str): The ID of the tracking entry.
+            **kwargs: Additional keyword arguments for updating the
+                tracking entry.
+
+        Raises:
+            TrackingDeletionError: If the tracking entry is not found.
+            TrackingCreationError: If no modifications are made to the
+                tracking entry.
+        """
         result = db.trackings.update_one(
             {"_id": tracking_id},
             {"$set": kwargs}
